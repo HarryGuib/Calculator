e47f914773e7857833ba79d465be1efc1a349341
HarryGuib <selim.ariguib@gmail.com>
2017-10-31 21:33:30 +0100

    added user input

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,10 @@ public class Calculator{
 		public static void main(String[] args)
 		{
 			Scanner scanner = new Scanner (System.in);
+			System.out.println("Bitte eingeben [+-*/] zahl1 zahl2");
+			String opreator = scanner.next();
+			int zahl1 = scanner.nextInt();
+			int zahl2 = scanner.nextInt();
 		}
 		
 }
